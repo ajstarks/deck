a95c4b752dd57c6407b0969d9d4d97858552c5c3
Anthony Starks <ajstarks@gmail.com>
2013-08-24 00:38:12 -0400

    update client doc.go files

--- pdfdeck/doc.go
+++ pdfdeck/doc.go
@@ -7,12 +7,19 @@ Usage
 	$ pdfdeck deck.xml  # make deck.pdf in current directory
 
 the -grid percent option draws a grid scaled to the specifed percentage on each slide
+
 the -fontdir option specifies the location of the font directory
+
 the -sans, -serif, and -mono options specify fonts
+
 the -outdir option specifies the directory where PDF files are written; defaults to the current directory
+
 the -author option adds author metadata
+
 the -title options adds title metadata
+
 the -pagesize option specifies the page dimensions (Letter, Legal, A3, A4, A5)
+
 the -stdout option specified that output goes to the standard output file
 */
 package main
---
+++ svgdeck/doc.go
@@ -0,0 +1,26 @@
+/*
+svgdeck is a program for making SVG slides using the deck package.
+
+Usage
+
+	$ go get github.com/ajstarks/deck/svgdeck
+	$ svgdeck deck.xml  # make deck-nnn.svg in current directory
+
+One SVG file per slide is generated in the output directory.  For example, a deck input file named "deck.xml"
+with 5 slides would generate deck-000.svg through deck-004.svg.
+
+the -grid percent option draws a grid scaled to the specifed percentage on each slide
+
+the -sans, -serif, and -mono options specify fonts
+
+the -outdir option specifies the directory where SVG files are written; defaults to the current directory
+
+the -title options adds title metadata
+
+the -pagesize option specifies the page dimensions (Letter, Legal, A3, A4, A5)
+
+the -stdout option specified that output goes to the standard output file
+
+the -w and -h options specify the slide canvas width and height
+*/
+package main
