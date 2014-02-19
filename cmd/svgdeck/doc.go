ede995188c20b066860cc8b97ff6977c3a5a9526
Anthony Starks <ajstarks@gmail.com>
2014-02-18 20:52:08 -0500

    more reorg

---
+++ cmd/svgdeck/doc.go
@@ -0,0 +1,28 @@
+/*
+svgdeck is a program for making SVG slides using the deck package.
+
+Usage
+
+	$ go get github.com/ajstarks/deck/svgdeck
+	$ svgdeck deck.xml  # make deck-nnn.svg in current directory
+
+One SVG file per slide is generated in the output directory.  For example, a deck input file named "deck.xml"
+with 5 slides would generate deck-000.svg through deck-004.svg.
+
+Clicking on any content will navigate to the next slide, cycling to the first slide when the last slide is reached.
+
+the -grid percent option draws a grid scaled to the specifed percentage on each slide.
+
+the -sans, -serif, and -mono options specify fonts.
+
+the -outdir option specifies the directory where SVG files are written; defaults to the current directory.
+
+the -title options adds title metadata.
+
+the -pagesize option specifies the page dimensions (Letter, Legal, A3, A4, A5).
+
+the -stdout option specified that output goes to the standard output file.
+
+the -pagewidth and -pageheight options specify the slide canvas width and height.
+*/
+package main
