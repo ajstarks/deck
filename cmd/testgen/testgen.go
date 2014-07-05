272288c2aff49ff848c74f07d2f320c88dae9cf0
Anthony Starks <ajstarks@gmail.com>
2014-07-05 02:04:23 -0400

    Add deck generation package with test client and example

---
+++ cmd/testgen/testgen.go
@@ -0,0 +1,353 @@
+package main
+
+import (
+	"fmt"
+	"github.com/ajstarks/deck/generate"
+	"math"
+	"math/rand"
+	"os"
+	"time"
+)
+
+const (
+	lorem   = `Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.`
+	hellogo = `package main
+
+import "fmt"
+func main() {
+	fmt.Println("hello, world")
+}`
+
+	hellorun = `$ go run hello.go
+hello, world`
+	rgbfmt = `rgb(%d,%d,%d)`
+)
+
+func randcolor() string {
+	return fmt.Sprintf(rgbfmt, rand.Intn(255), rand.Intn(255), rand.Intn(255))
+}
+
+func randp(n float64) float64 {
+	x := math.Ceil(rand.Float64() * n)
+	if x == 0 {
+		return 1
+	}
+	return x
+}
+
+func randpoly(cx, cy, size float64, np int) (string, string) {
+	rx, ry := rp(cx, cy, size, np)
+	return generate.Polycoord(rx, ry)
+}
+
+func rp(cx, cy, size float64, np int) ([]float64, []float64) {
+	adiv := 360.0 / float64(np)
+	rx := make([]float64, np)
+	ry := make([]float64, np)
+	a := 0.0
+	for i := 0; i < np; i++ {
+		t := a * (math.Pi / 180)
+		r := randp(size)
+		rx[i] = (r * math.Cos(t)) + cx
+		ry[i] = (r * math.Sin(t)) + cy
+		a += adiv
+	}
+	return rx, ry
+}
+
+func section(p *generate.Deck, name string, n int) {
+	x := 10.0
+	y := 70.0
+	size := 5.0
+
+	ry := y - (size / 2)
+	p.Text(x, y, name, size, "")
+	p.Line(x, ry, 100-x, ry, 0.1, "black")
+	p.Text(80, 10, fmt.Sprintf("%d", n), size/3, "gray")
+	list := []string{}
+	for i := 0; i < 5; i++ {
+		list = append(list, fmt.Sprintf("item number %d", (i+1)*(n*10)))
+	}
+	var ltype string
+	switch {
+	case n%2 == 0:
+		ltype = "bullet"
+	case n%3 == 0:
+		ltype = "number"
+	default:
+		ltype = "plain"
+	}
+	p.List(x, 60, size/2, list, ltype, "")
+}
+
+func fitlist(p *generate.Deck, x, y, size float64, list []string) {
+	yp := y
+	colsize := 35.0
+	interval := y/float64(len(list)) + size
+	for _, s := range list {
+		p.Text(x, yp, s, size, "black")
+		yp -= interval
+		if yp < size {
+			x += colsize
+			yp = y
+		}
+	}
+}
+
+func rarrow(p *generate.Deck, x, y, w, h, aw, ah float64, color string, opacity float64) {
+	xw := x - w
+	xa := x - aw
+	h2 := h / 2
+	ah2 := ah / 2
+	px := []float64{xw, xa, xa, x, xa, xa, xw, xw}
+	py := []float64{y + h2, y + h2, y + ah2, y, y - ah2, y - h2, y - h2, y + h2}
+	p.Polygon(px, py, color, opacity)
+}
+
+func larrow(p *generate.Deck, x, y, w, h, aw, ah float64, color string, opacity float64) {
+	xw := x + w
+	xa := x + aw
+	h2 := h / 2
+	ah2 := ah / 2
+	px := []float64{xw, xa, xa, x, xa, xa, xw, xw}
+	py := []float64{y + h2, y + h2, y + ah2, y, y - ah2, y - h2, y - h2, y + h2}
+	p.Polygon(px, py, color, opacity)
+}
+
+func darrow(p *generate.Deck, x, y, w, h, aw, ah float64, color string, opacity float64) {
+	w2 := w / 2
+	aw2 := aw / 2
+	px := []float64{x, x + aw2, x + w2, x + w2, x - w2, x - w2, x - aw2, x}
+	py := []float64{y, y + ah, y + ah, y + h, y + h, y + ah, y + ah, y}
+	p.Polygon(px, py, color, opacity)
+
+}
+
+func uarrow(p *generate.Deck, x, y, w, h, aw, ah float64, color string, opacity float64) {
+	w2 := w / 2
+	aw2 := aw / 2
+	px := []float64{x, x - aw2, x - w2, x - w2, x + w2, x + w2, x + aw2, x}
+	py := []float64{y, y - ah, y - ah, y - h, y - h, y - ah, y - ah, y}
+	p.Polygon(px, py, color, opacity)
+}
+
+// Test slide generation
+func main() {
+	rand.Seed(time.Now().Unix() % 1e9)
+
+	n := 200
+	sections := []string{"One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten"}
+
+	deck := generate.NewSlides(os.Stdout, 1600, 900)
+	deck.StartDeck()
+
+	// Text
+	deck.StartSlide()
+	for i := 0; i < n; i++ {
+		deck.TextMid(randp(100), randp(100), "hello", randp(10), randcolor(), randp(100))
+	}
+	deck.EndSlide()
+
+	// Text Block
+	deck.StartSlide("rgb(180,180,180)")
+	deck.TextBlock(10, 90, lorem, 2.5, 25, "black")
+	deck.TextBlock(10, 50, lorem, 2.5, 50, "gray")
+	deck.TextBlock(10, 20, lorem, 2.5, 75, "white")
+	deck.EndSlide()
+
+	// Text alignment
+	deck.StartSlide("rgb(180,180,180)")
+	deck.Text(50, 80, "left", 10, "black")
+	deck.TextMid(50, 50, "center", 10, "gray")
+	deck.TextEnd(50, 20, "right", 10, "white")
+	deck.Line(50, 100, 50, 0, 0.2, "black", 20)
+	deck.EndSlide()
+
+	// Code
+	deck.StartSlide("rgb(180,180,180)")
+	deck.Code(35, 80, "$ edit hello.go", 2, 40, "rgb(0,0,127)")
+	deck.Code(35, 70, hellogo, 2, 40, "black")
+	deck.Code(35, 35, hellorun, 2, 40, "rgb(127,0,0)")
+	deck.EndSlide()
+
+	// Fit text
+	deck.StartSlide()
+	fitlist(deck, 10, 70, 5, sections)
+	deck.EndSlide()
+
+	// Lists
+	for i := 0; i < 3; i++ {
+		deck.StartSlide()
+		section(deck, sections[i], i+1)
+		deck.EndSlide()
+	}
+
+	// Image
+	deck.StartSlide("gray")
+	for i := 0; i < n; i++ {
+		deck.Image(randp(100), randp(100), 100, 100, "sm.png")
+	}
+	deck.EndSlide()
+
+	// Circle
+	deck.StartSlide()
+	for i := 0; i < n; i++ {
+		deck.Circle(randp(100), randp(100), randp(10), randcolor(), randp(100))
+	}
+	deck.EndSlide()
+
+	// Ellipse
+	deck.StartSlide()
+	for i := 0; i < n; i++ {
+		deck.Ellipse(randp(100), randp(100), randp(10), randp(10), randcolor(), randp(100))
+	}
+	deck.EndSlide()
+
+	// Square
+	deck.StartSlide()
+	for i := 0; i < n; i++ {
+		deck.Square(randp(100), randp(100), randp(10), randcolor(), randp(100))
+	}
+	deck.EndSlide()
+
+	// Rect
+	deck.StartSlide()
+	for i := 0; i < n; i++ {
+		deck.Rect(randp(100), randp(100), randp(10), randp(10), randcolor(), randp(100))
+	}
+	deck.EndSlide()
+
+	// Arc
+	deck.StartSlide()
+	for i := 0; i < n; i++ {
+		deck.Arc(randp(100), randp(100), randp(10), randp(10), 0.5, randp(90), randp(300), randcolor(), randp(100))
+	}
+	deck.EndSlide()
+
+	// Curve
+	deck.StartSlide()
+	for i := 0; i < n; i++ {
+		deck.Curve(randp(100), randp(100), randp(100), randp(100), randp(100), randp(100), 0.5, randcolor(), randp(100))
+	}
+	deck.EndSlide()
+
+	// Line
+	deck.StartSlide()
+	for i := 0; i < n; i++ {
+		x := randp(100)
+		y := randp(100)
+		if i%2 == 0 {
+			deck.Line(x, y, x+randp(10), y, 0.5, randcolor(), randp(100))
+		} else {
+			deck.Line(x, y, x, y+randp(10), 0.5, randcolor(), randp(100))
+		}
+	}
+	deck.EndSlide()
+	
+	// Polygon
+	deck.StartSlide()
+	for i := 0; i < n; i++ {
+		px, py := rp(randp(100), randp(100), randp(10), 5)
+		deck.Polygon(px, py, randcolor(), randp(100))
+	}
+	deck.EndSlide()
+
+	// Left arrow
+	deck.StartSlide()
+	for i := 0; i < n; i++ {
+		x := randp(100)
+		y := randp(100)
+		w := randp(50)
+		h := 3.0
+		aw := h * 2
+		ah := aw * 0.9
+		larrow(deck, x, y, w, h, aw, ah, randcolor(), randp(100))
+	}
+	deck.EndSlide()
+
+	// Right arrow
+	deck.StartSlide()
+	for i := 0; i < n; i++ {
+		x := randp(100)
+		y := randp(100)
+		w := randp(50)
+		h := 3.0
+		aw := h * 2
+		ah := aw * 0.9
+		rarrow(deck, x, y, w, h, aw, ah, randcolor(), randp(100))
+	}
+	deck.EndSlide()
+
+	// Up arrow
+	deck.StartSlide()
+	for i := 0; i < n; i++ {
+		x := randp(100)
+		y := randp(100)
+		w := 3.0
+		h := randp(50)
+		aw := w * 2
+		ah := aw * 0.9
+		uarrow(deck, x, y, w, h, aw, ah, randcolor(), randp(100))
+	}
+	deck.EndSlide()
+
+	// Down arrow
+	deck.StartSlide()
+	for i := 0; i < n; i++ {
+		x := randp(100)
+		y := randp(100)
+		w := 3.0
+		h := randp(50)
+		aw := w * 2
+		ah := aw * 0.9
+		darrow(deck, x, y, w, h, aw, ah, randcolor(), randp(100))
+
+	}
+	deck.EndSlide()
+
+	// Left and right arrow
+	deck.StartSlide()
+	for i := 0; i < n; i++ {
+		x := randp(100)
+		y := randp(100)
+		w := randp(50)
+		h := 3.0
+		aw := h * 2
+		ah := aw * 0.9
+		if i%2 == 0 {
+			larrow(deck, x, y, w, h, aw, ah, randcolor(), randp(100))
+		} else {
+			rarrow(deck, x, y, w, h, aw, ah, randcolor(), randp(100))
+		}
+	}
+	deck.EndSlide()
+
+	// Up and down arrow
+	deck.StartSlide()
+	for i := 0; i < n; i++ {
+		x := randp(100)
+		y := randp(100)
+		w := 3.0
+		h := randp(50)
+		aw := w * 2
+		ah := aw * 0.9
+		if i%2 == 0 {
+			uarrow(deck, x, y, w, h, aw, ah, randcolor(), randp(100))
+		} else {
+			darrow(deck, x, y, w, h, aw, ah, randcolor(), randp(100))
+		}
+	}
+	deck.EndSlide()
+
+	// Diagram
+	deck.StartSlide()
+	deck.Rect(25, 50, 20, 15, "red")
+	deck.Rect(75, 50, 20, 15, "green")
+	rarrow(deck, 50, 50, 20, 2, 2, 5, "red", 40)
+	larrow(deck, 50, 50, 20, 2, 2, 5, "green", 40)
+	deck.TextMid(50, 40, "conflict", 3, "black")
+	deck.TextMid(25, 48, "Urgent", 4, "white")
+	deck.TextMid(75, 48, "Important", 4, "white")
+	deck.EndSlide()
+	deck.EndDeck()
+}
---
+++ generate/generate.go
@@ -0,0 +1,338 @@
+// package generate performs slide deck generation
+package generate
+
+import (
+	"fmt"
+	"github.com/ajstarks/deck"
+	"io"
+)
+
+const (
+	circlefmt  = `<ellipse xp="%.2f" yp="%.2f" wp="%.2f" hr="%.2f" opacity="%.2f" color="%s"/>`
+	squarefmt  = `<rect xp="%.2f" yp="%.2f" wp="%.2f" hr="%.2f" opacity="%.2f" color="%s"/>`
+	ellipsefmt = `<ellipse xp="%.2f" yp="%.2f" wp="%.2f" hp="%.2f" opacity="%.2f" color="%s"/>`
+	rectfmt    = `<rect xp="%.2f" yp="%.2f" wp="%.2f" hp="%.2f" opacity="%.2f" color="%s"/>`
+	arcfmt     = `<arc xp="%.2f" yp="%.2f" wp="%.2f" hp="%.2f" sp="%.2f" a1="%.2f" a2="%.2f" opacity="%.2f" color="%s"/>`
+	linefmt    = `<line xp1="%.2f" yp1="%.2f" xp2="%.2f" yp2="%.2f" sp="%.2f" opacity="%.2f" color="%s"/>`
+	curvefmt   = `<curve xp1="%.2f" yp1="%.2f" xp2="%.2f" yp2="%.2f" xp3="%.2f" yp3="%.2f" sp="%.2f" opacity="%.2f" color="%s"/>`
+	polygonfmt = `<polygon xc="%s" yc="%s" opacity="%.2f" color="%s"/>`
+	textfmt    = `<text xp="%.2f" yp="%.2f" sp="%.2f" align="%s" wp="%.2f" opacity="%.2f" color="%s" type="%s">%s</text>`
+	imagefmt   = `<image xp="%.2f" yp="%.2f" width="%d" height="%d" name="%s"/>`
+	listfmt    = `<list type="%s" xp="%.2f" yp="%.2f" sp="%.2f" color="%s">`
+	lifmt      = `<li>%s</li>`
+	closelist  = `</list>`
+	slidefmt   = `<slide>`
+	slidebg    = `<slide bg="%s">`
+	slidebgfg  = `<slide bg="%s" fg="%s">`
+	closeslide = `</slide>`
+	deckfmt    = `<deck><canvas width="%d" height="%d"/>`
+	closedeck  = `</deck>`
+)
+
+// Deck is the generated deck structure.
+type Deck struct {
+	width, height int
+	dest          io.Writer
+}
+
+// NewSlides initializes he generated deck structure.
+func NewSlides(where io.Writer, w, h int) *Deck {
+	return &Deck{dest: where, width: w, height: h}
+}
+
+// StartDeck begins a slide deck.
+func (p *Deck) StartDeck() {
+	fmt.Fprintf(p.dest, deckfmt, p.width, p.height)
+}
+
+// EndDeck ends a slide.
+func (p *Deck) EndDeck() {
+	fmt.Fprintln(p.dest, closedeck)
+}
+
+// Startslide begins a slide.
+func (p *Deck) StartSlide(colors ...string) {
+	switch len(colors) {
+	case 1:
+		fmt.Fprintf(p.dest, slidebg, colors[0])
+	case 2:
+		fmt.Fprintf(p.dest, slidebgfg, colors[0], colors[1])
+	default:
+		fmt.Fprintln(p.dest, slidefmt)
+	}
+}
+
+// Endslide ends a slide.
+func (p *Deck) EndSlide() {
+	fmt.Fprintln(p.dest, closeslide)
+}
+
+// square makes  square markup from the rect structure.
+func (p *Deck) square(r deck.Rect) {
+	fmt.Fprintf(p.dest, squarefmt, r.Xp, r.Yp, r.Wp, r.Hr, r.Opacity, r.Color)
+}
+
+// circle makes square markup from the ellipse structure.
+func (p *Deck) circle(e deck.Ellipse) {
+	fmt.Fprintf(p.dest, circlefmt, e.Xp, e.Yp, e.Wp, e.Hr, e.Opacity, e.Color)
+}
+
+// ellipse makes ellipse markup from the ellipse structure.
+func (p *Deck) ellipse(e deck.Ellipse) {
+	fmt.Fprintf(p.dest, ellipsefmt, e.Xp, e.Yp, e.Wp, e.Hp, e.Opacity, e.Color)
+}
+
+// rect makes rect markup rom the rect structure.
+func (p *Deck) rect(r deck.Rect) {
+	fmt.Fprintf(p.dest, rectfmt, r.Xp, r.Yp, r.Wp, r.Hp, r.Opacity, r.Color)
+}
+
+// line makes line markup from the deck line structure.
+func (p *Deck) line(l deck.Line) {
+	fmt.Fprintf(p.dest, linefmt, l.Xp1, l.Yp1, l.Xp2, l.Yp2, l.Sp, l.Opacity, l.Color)
+}
+
+// curve makes curve markup from the curve structure.
+func (p *Deck) curve(c deck.Curve) {
+	fmt.Fprintf(p.dest, curvefmt, c.Xp1, c.Yp1, c.Xp2, c.Yp2, c.Xp3, c.Yp3, c.Sp, c.Opacity, c.Color)
+}
+
+// arc makes arc markup from the arc structure.
+func (p *Deck) arc(a deck.Arc) {
+	fmt.Fprintf(p.dest, arcfmt, a.Xp, a.Yp, a.Wp, a.Hp, a.Sp, a.A1, a.A2, a.Opacity, a.Color)
+}
+
+// polygon makes polygon markup from the polygon structure.
+func (p *Deck) polygon(poly deck.Polygon) {
+	fmt.Fprintf(p.dest, polygonfmt, poly.XC, poly.YC, poly.Opacity, poly.Color)
+}
+
+// text makes text markup from the deck text structure.
+func (p *Deck) text(t deck.Text) {
+	fmt.Fprintf(p.dest, textfmt, t.Xp, t.Yp, t.Sp, t.Align, t.Wp, t.Opacity, t.Color, t.Type, t.Tdata)
+}
+
+// image makes image markup from the deck image structure.
+func (p *Deck) image(pic deck.Image) {
+	fmt.Fprintf(p.dest, imagefmt, pic.Xp, pic.Yp, pic.Width, pic.Height, pic.Name)
+}
+
+// list makes markup from the list deck structure.
+func (p *Deck) list(l deck.List, items []string, ltype string) {
+	fmt.Fprintf(p.dest, listfmt, ltype, l.Xp, l.Yp, l.Sp, l.Color)
+	for _, s := range items {
+		fmt.Fprintf(p.dest, lifmt, s)
+	}
+	fmt.Fprintln(p.dest, closelist)
+}
+
+// Text places plain text aligned at (x,y), with specified size and color. Opacity is optional
+func (p *Deck) Text(x, y float64, s string, size float64, color string, opacity ...float64) {
+	t := deck.Text{}
+	t.Xp = x
+	t.Yp = y
+	t.Sp = size
+	t.Color = color
+	t.Tdata = s
+	if len(opacity) > 0 {
+		t.Opacity = opacity[0]
+	}
+	p.text(t)
+}
+
+// TextMid places centered text aligned at (x,y), with specified size and color. Opacity is optional.
+func (p *Deck) TextMid(x, y float64, s string, size float64, color string, opacity ...float64) {
+	t := deck.Text{}
+	t.Xp = x
+	t.Yp = y
+	t.Sp = size
+	t.Tdata = s
+	t.Color = color
+	t.Align = "center"
+	if len(opacity) > 0 {
+		t.Opacity = opacity[0]
+	}
+	p.text(t)
+}
+
+// TextEnd places right-justified text aligned at (x,y), with specified size and color. Opacity is optional.
+func (p *Deck) TextEnd(x, y float64, s string, size float64, color string, opacity ...float64) {
+	t := deck.Text{}
+	t.Xp = x
+	t.Yp = y
+	t.Sp = size
+	t.Tdata = s
+	t.Color = color
+	t.Align = "right"
+	if len(opacity) > 0 {
+		t.Opacity = opacity[0]
+	}
+	p.text(t)
+}
+
+// TextBlock makes a block of text aligned at (x,y), wrapped at margin; with specified size and color. Opacity is optional.
+func (p *Deck) TextBlock(x, y float64, s string, size, margin float64, color string, opacity ...float64) {
+	t := deck.Text{}
+	t.Xp = x
+	t.Yp = y
+	t.Sp = size
+	t.Wp = margin
+	t.Tdata = s
+	t.Color = color
+	t.Type = "block"
+	if len(opacity) > 0 {
+		t.Opacity = opacity[0]
+	}
+	p.text(t)
+}
+
+// Code makes a code block at (x,y), with specified size and color (opacity is optional),
+// on a light gray background with the specified margin width.
+func (p *Deck) Code(x, y float64, s string, size, margin float64, color string, opacity ...float64) {
+	t := deck.Text{}
+	t.Xp = x
+	t.Yp = y
+	t.Sp = size
+	t.Wp = margin
+	t.Tdata = s
+	t.Color = color
+	t.Type = "code"
+	if len(opacity) > 0 {
+		t.Opacity = opacity[0]
+	}
+	p.text(t)
+}
+
+// List makes a plain, bullet, or plain list with the specified size and color.
+func (p *Deck) List(x, y, size float64, items []string, ltype, color string) {
+	l := deck.List{}
+	l.Xp = x
+	l.Yp = y
+	l.Sp = size
+	l.Color = color
+	p.list(l, items, ltype)
+}
+
+// Square makes a square, centered at (x,y), with width w, at the specified color and optional opacity.
+func (p *Deck) Square(x, y, w float64, color string, opacity ...float64) {
+	r := deck.Rect{}
+	r.Xp = x
+	r.Yp = y
+	r.Wp = w
+	r.Hr = 100
+	r.Color = color
+	if len(opacity) > 0 {
+		r.Opacity = opacity[0]
+	}
+	p.square(r)
+}
+
+// Circle makes a circle, centered at (x,y) with width w, at the specified color and optional opacity.
+func (p *Deck) Circle(x, y, w float64, color string, opacity ...float64) {
+	r := deck.Ellipse{}
+	r.Xp = x
+	r.Yp = y
+	r.Wp = w
+	r.Hr = 100
+	r.Color = color
+	if len(opacity) > 0 {
+		r.Opacity = opacity[0]
+	}
+	p.circle(r)
+}
+
+// Rect makes a rectangle, centered at (x,y), with (w,h) dimensions, at the specified color and optional opacity.
+func (p *Deck) Rect(x, y, w, h float64, color string, opacity ...float64) {
+	r := deck.Rect{}
+	r.Xp = x
+	r.Yp = y
+	r.Wp = w
+	r.Hp = h
+	r.Color = color
+	if len(opacity) > 0 {
+		r.Opacity = opacity[0]
+	}
+	p.rect(r)
+}
+
+// Ellipse makes a ellipse graphic, centered at (x,y), with (w,h) dimensions, at the specified color and optional opacity.
+func (p *Deck) Ellipse(x, y, w, h float64, color string, opacity ...float64) {
+	r := deck.Ellipse{}
+	r.Xp = x
+	r.Yp = y
+	r.Wp = w
+	r.Hp = h
+	r.Color = color
+	if len(opacity) > 0 {
+		r.Opacity = opacity[0]
+	}
+	p.ellipse(r)
+}
+
+// Line makes a line from (x1,y1) to (x2, y2), with the specified color with optional opacity; thickness is size.
+func (p *Deck) Line(x1, y1, x2, y2, size float64, color string, opacity ...float64) {
+	l := deck.Line{Xp1: x1, Xp2: x2, Yp1: y1, Yp2: y2, Sp: size, Color: color}
+	if len(opacity) > 0 {
+		l.Opacity = opacity[0]
+	}
+	p.line(l)
+}
+
+// Arc makes an arc centered at (x,y), with specified color (with optional opacity),
+// with dimensions (w,h), between angle a1 and a2 (specified in degrees).
+func (p *Deck) Arc(x, y, w, h, size, a1, a2 float64, color string, opacity ...float64) {
+	a := deck.Arc{A1: a1, A2: a2}
+	a.Xp = x
+	a.Yp = y
+	a.Wp = w
+	a.Hp = h
+	a.Sp = size
+	a.Color = color
+	if len(opacity) > 0 {
+		a.Opacity = opacity[0]
+	}
+	p.arc(a)
+}
+
+// Curve makes a Bezier curve between (x1, y2) and (x3, y3), with control points at (x2, y2), thickness is specified by size.
+func (p *Deck) Curve(x1, y1, x2, y2, x3, y3, size float64, color string, opacity ...float64) {
+	c := deck.Curve{Xp1: x1, Xp2: x2, Xp3: x3, Yp1: y1, Yp2: y2, Yp3: y3, Sp: size, Color: color}
+	if len(opacity) > 0 {
+		c.Opacity = opacity[0]
+	}
+	p.curve(c)
+}
+
+// Polygon makes a polygon with the specified color (with optional opacity), with coordinates in x and y slices.
+func (p *Deck) Polygon(x, y []float64, color string, opacity ...float64) {
+	xc, yc := Polycoord(x, y)
+	poly := deck.Polygon{XC: xc, YC: yc, Color: color}
+	if len(opacity) > 0 {
+		poly.Opacity = opacity[0]
+	}
+	p.polygon(poly)
+}
+
+// Polycoord converts slices of coordinates to strings.
+func Polycoord(px, py []float64) (string, string) {
+	var xc, yc string
+	np := len(px)
+	if np < 3 || len(py) != np {
+		return xc, yc
+	}
+	for i := 0; i < np-1; i++ {
+		xc += fmt.Sprintf("%.2f ", px[i])
+		yc += fmt.Sprintf("%.2f ", py[i])
+	}
+	xc += fmt.Sprintf("%.2f", px[np-1])
+	yc += fmt.Sprintf("%.2f", py[np-1])
+	return xc, yc
+}
+
+// Image places the named image centered at (x, y), with dimensions of (w, h).
+func (p *Deck) Image(x, y float64, w, h int, name string) {
+	i := deck.Image{Width: w, Height: h, Name: name}
+	i.Xp = x
+	i.Yp = y
+	p.image(i)
+}
