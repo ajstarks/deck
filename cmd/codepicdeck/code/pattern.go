e18a9cbeb198457574dcd4965f7c4bc8d7e9e797
Anthony Starks <ajstarks@gmail.com>
2015-09-08 21:35:06 -0400

    cmd/codepicdeck: update pattern

--- cmd/codepicdeck/code/pattern.go
+++ cmd/codepicdeck/code/pattern.go
@@ -15,7 +15,6 @@ func main() {
 	pw, ph := (width*pct)/100, (height*pct)/100
 	canvas.Start(width, height)
 
-	// define the pattern
 	canvas.Def()
 	canvas.Pattern("hatch", 0, 0, pw, ph, "user")
 	canvas.Gstyle("fill:none;stroke-width:1")
@@ -25,10 +24,13 @@ func main() {
 	canvas.PatternEnd()
 	canvas.DefEnd()
 
-	// use the pattern
-	canvas.Gstyle("stroke:black; stroke-width:2")
-	canvas.Circle(width/2, height/2, height/8, "fill:url(#hatch)")
-	canvas.CenterRect((width*4)/5, height/2, height/4, height/4, "fill:url(#hatch)")
+	x1 := width/2
+	x2 := (width*4)/5
+	canvas.Gstyle("stroke:black; font-size: 72pt; text-anchor:middle; fill:url(#hatch)")
+	canvas.Circle(x1, height/2, height/8)
+	canvas.CenterRect(x2, height/2, height/4, height/4)
+	canvas.Text(x1, height-50, "Go")
+	canvas.Text(x2, height-50, "fast")
 	canvas.Gend()
 	canvas.End()
 }
