19bbae61237f916209fa6c41fcb54266a3587369
Anthony Starks <ajstarks@gmail.com>
2015-09-13 10:12:11 -0400

    update imfade

--- cmd/codepicdeck/code/imfade.go
+++ cmd/codepicdeck/code/imfade.go
@@ -17,7 +17,7 @@ func main() {
 	canvas.Start(width, height)
 	opacity := 1.0
 	for x := 0; x < width; x += 100 {
-		canvas.Image(x, 100, 122, 172, "gopher.png", fmt.Sprintf("opacity:%.2f", opacity))
+		canvas.Image(x, 100, 100, 124, "gopher.jpg", fmt.Sprintf("opacity:%.2f", opacity))
 		opacity -= 0.2
 	}
 	canvas.End()
