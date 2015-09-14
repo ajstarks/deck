b19e6c1fb32f591b4ffafe87deffdc0ae33f53a0
Anthony Starks <ajstarks@gmail.com>
2015-09-13 21:07:17 -0400

    cmd/codepicdeck: use Symbola font, better range for uni

--- cmd/codepicdeck/code/uni.go
+++ cmd/codepicdeck/code/uni.go
@@ -15,14 +15,14 @@ var (
 
 func main() {
 	top, left, fontsize := 50, 100, 16
-	xoffset, yoffset := 26, 26
+	xoffset, yoffset := 25, 25
 	rows, cols := 16, 16
-	glyph := 0x2700
-	font := "Calibri"
+	glyph := 0x2600
+	font := "Symbola"
 	stylefmt := "font-family:%s;font-size:%dpx;text-anchor:middle"
 	canvas.Start(width, height)
 	canvas.Gstyle(fmt.Sprintf(stylefmt, font, fontsize))
-	
+
 	x, y := left, top
 	for r := 0; r < rows; r++ {
 		canvas.Text(x-yoffset, y, fmt.Sprintf("%X", glyph), "text-anchor:end;fill:gray")
