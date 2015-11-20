e240352ee98eb06aeb576b4c1a8814a13e3997da
Anthony Starks <ajstarks@gmail.com>
2015-11-20 06:56:10 -0500

    codepicdeck: update artist

--- cmd/codepicdeck/code/artist.go
+++ cmd/codepicdeck/code/artist.go
@@ -18,18 +18,18 @@ func tf(x, y int, s string, size float64) {
 }
 
 func main() {
-	x, y := 50, 40
+	x, y := width/2, 35
 	canvas.Start(width, height)
 	canvas.Rect(0,0,width,height, canvas.RGB(72, 45, 77))
-	canvas.Gstyle("font-family:Roboto;fill:white")
-	tf(x, y, "A MAN WHO WORKS WITH HIS HANDS IS A LABORER",12); y += 70
-	tf(x, y, "A MAN WHO", 52); y += 100
-	tf(x, y, "WORKS", 87); y += 40
-	tf(x, y, "WITH HIS HANDS AND HIS BRAIN IS A CRAFTSMAN", 13); y += 50
-	tf(x, y, "BUT A MAN WHO",37); y += 30
-	tf(x, y, "WORKS WITH HIS HANDS AND HIS BRAIN", 15); y += 50
-	tf(x, y, "AND HIS HEART IS", 35.4); y += 80
-	tf(x, y, "AN ARTIST", 60)
+	canvas.Gstyle("font-family:Roboto;fill:white;text-anchor:middle")
+	tf(x, y, "A MAN WHO WORKS WITH HIS HANDS IS A LABORER",14); y += 70
+	tf(x, y, "A MAN WHO", 60); y += 105
+	tf(x, y, "WORKS", 90); y += 35
+	tf(x, y, "WITH HIS HANDS AND HIS BRAIN IS A CRAFTSMAN", 15); y += 60
+	tf(x, y, "BUT A MAN WHO",42); y += 40
+	tf(x, y, "WORKS WITH HIS HANDS AND HIS BRAIN", 16); y += 55
+	tf(x, y, "AND HIS HEART IS", 36); y += 85
+	tf(x, y, "AN ARTIST", 64)
 	canvas.Gend()
 	canvas.End()
 }
