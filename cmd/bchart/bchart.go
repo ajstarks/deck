a38a27cda5d5284b32b9ad0ce252d7029efc7795
Anthony Starks <ajstarks@gmail.com>
2017-11-28 21:32:59 -0500

    bchart: add dotted line, comments, cleanup

--- cmd/bchart/bchart.go
+++ cmd/bchart/bchart.go
@@ -24,7 +24,7 @@
 //	  -dmin zero minimum
 //	  -dot draw a line and dot instead of a solid bar
 //	  -left left margin (default 20)
-//	  -textsize text size (default 1.2)
+//	  -textsize text size (default 1.5)
 //	  -top top of the chart (default 90)
 package main
 
@@ -45,10 +45,13 @@ type Bardata struct {
 	value float64
 }
 
+// vmap maps one range into another
 func vmap(value float64, low1 float64, high1 float64, low2 float64, high2 float64) float64 {
 	return low2 + (high2-low2)*(value-low1)/(high1-low1)
 }
 
+// getdata name,value pairs, with optional comments,
+// returning a slice with the data, allong with min, max and title
 func getdata(r io.Reader) ([]Bardata, float64, float64, string) {
 	var (
 		data []Bardata
@@ -57,13 +60,13 @@ func getdata(r io.Reader) ([]Bardata, float64, float64, string) {
 	)
 
 	maxval := -1.0
-	minval := 1e50
+	minval := 1e308
 	title := ""
 	scanner := bufio.NewScanner(r)
 	for scanner.Scan() {
 		t := scanner.Text()
 		if t[0] == '#' && len(t) > 2 {
-			title = t[1:]
+			title = strings.TrimSpace(t[1:])
 			continue
 		}
 		fields := strings.Split(t, "\t")
@@ -85,12 +88,17 @@ func getdata(r io.Reader) ([]Bardata, float64, float64, string) {
 	}
 	return data, minval, maxval, title
 }
+
+// dottedline makes a dotted horizontal line, using circles,
+// with specified step and separation
+func dottedline(d *generate.Deck, x, y, width, height, step, space float64, color string) {
+	for xp := x; xp < x+width; xp += step {
+		d.Circle(xp, y, height, color)
+		xp += space
+	}
+}
+
 func main() {
-	var (
-		ts, left, top      float64
-		dot, datamin       bool
-		datacolor, datafmt string
-	)
 	const (
 		bgcolor      = "white"
 		titlecolor   = "black"
@@ -98,10 +106,15 @@ func main() {
 		dotlinecolor = "lightgray"
 		valuecolor   = "rgb(127,0,0)"
 	)
-	flag.Float64Var(&ts, "textsize", 1.2, "text size")
+	var (
+		ts, left, top      float64
+		dot, datamin       bool
+		datacolor, datafmt string
+	)
+	flag.Float64Var(&ts, "textsize", 1.5, "text size")
 	flag.Float64Var(&left, "left", 20.0, "left margin")
 	flag.Float64Var(&top, "top", 90.0, "top")
-	flag.BoolVar(&dot, "dot", false, "dot and line")
+	flag.BoolVar(&dot, "dot", false, "dot")
 	flag.BoolVar(&datamin, "dmin", false, "zero minimum")
 	flag.StringVar(&datacolor, "color", "rgb(175,175,175)", "bar color")
 	flag.StringVar(&datafmt, "datafmt", "%.1f", "data format")
@@ -118,20 +131,22 @@ func main() {
 	deck := generate.NewSlides(os.Stdout, 0, 0)
 	deck.StartDeck()
 	deck.StartSlide(bgcolor)
-	if title != "" {
-		deck.TextMid(50, top+(linespacing*1.5), title, "serif", ts*1.5, titlecolor)
+	if len(title) > 0 {
+		deck.TextMid(50, top+(linespacing*1.5), title, "sans", ts*1.5, titlecolor)
 	}
+
+	// for every name, value pair, make the chart
 	y := top
 	for _, data := range bardata {
 		deck.TextEnd(left-hts, y, data.label, "sans", ts, labelcolor)
 		bv := vmap(data.value, mindata, maxdata, left, right)
 		if dot {
-			deck.Line(left, y+hts, bv, y+hts, ts/4, dotlinecolor)
+			dottedline(deck, left, y+hts, bv-left, ts/5, 1, 0.5, dotlinecolor)
 			deck.Circle(bv, y+hts, hts, datacolor)
 		} else {
 			deck.Line(left, y+hts, bv, y+hts, ts, datacolor)
 		}
-		deck.Text(bv+hts, y+(hts/2), fmt.Sprintf(datafmt, data.value), "mono", ts*0.65, valuecolor)
+		deck.Text(bv+hts, y+(hts/2), fmt.Sprintf(datafmt, data.value), "mono", ts*0.75, valuecolor)
 		y -= linespacing
 	}
 	deck.EndSlide()
