7ec3be902eff7bd71eede5f84ad00eebd7eb45b9
Anthony Starks <ajstarks@gmail.com>
2017-12-23 15:30:46 -0500

    lchart: update doc.go

--- cmd/lchart/doc.go
+++ cmd/lchart/doc.go
@@ -1,5 +1,5 @@
-//
 package main
+
 // lchart reads data from the standard input file, or specified files, expecting a tab-separated list of text,data pairs
 // where text is an arbitrary string, and data is intepreted as a floating point value.
 // A line beginning with "#" is parsed as a title, with the title text beginning after the "#".
@@ -19,21 +19,21 @@ package main
 //
 // The command line options are:
 //
-// -dmim		zero minimum (default false)
-// -bar			show bars (default true)
-// -connect		connect data points (default false)
-// -dot			show dot plot (default false)
-// -grid		show gridlines on the y axis (default false)
-// -val			show values (default true)
-// -vol			show volume plot (default false)
-// -top			top of the plot (default 80)
-// -bottom 		bottom of the plot (default 30)
-// -left		left margin (default 10)
-// -right		right margin (default 90)
-// -barwidth	barwidth (default computed from the number of data points)
-// -ls			linespacing (default 2.4)
-// -textsize	text size (default 1.5)
-// -xlabel		x axis label interval (default 1)
-// -yaxis		show a y axis
-// -color 		data color (default "lightsteelblue")
-// -datafmt		data format (default "%.1f")
+//	-dmim		zero minimum (default false)
+//	-bar		show bars (default true)
+//	-connect	connect data points (default false)
+//	-dot		show dot plot (default false)
+//	-grid		show gridlines on the y axis (default false)
+//	-val		show values (default true)
+//	-vol		show volume plot (default false)
+//	-top		top of the plot (default 80)
+//	-bottom 	bottom of the plot (default 30)
+//	-left		left margin (default 10)
+//	-right		right margin (default 90)
+//	-barwidth	barwidth (default computed from the number of data points)
+//	-ls		linespacing (default 2.4)
+//	-textsize	text size (default 1.5)
+//	-xlabel		x axis label interval (default 1)
+//	-yaxis		show a y axis
+//	-color 		data color (default "lightsteelblue")
+//	-datafmt	data format (default "%.1f")
