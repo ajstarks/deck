75913997d7976539037e777debb109fc901601d4
Anthony Starks <ajstarks@gmail.com>
2018-10-07 03:40:37 -0400

    reformat eval.sh

--- cmd/decksh/eval.sh
+++ cmd/decksh/eval.sh
@@ -1,29 +1,32 @@
 // comprehensive tests
-l1=20
-l2=50
-l3=80
-op1=70
-op2=50
-op3=30
-ts1=4
-ty=92
-tb="Now is the time for all good men to come to the aid of the party & 'do it now'"
-
 deck begin
 	canvas 1200 900
+	tx1=5
+	tx2=35
+	tx3=65
+	ty=92
+	tb="Now is the time for all good men to come to the aid of the party & 'do it now'"
+	// Text Functions
 	slide begin
-		textblock tb  5 ty 20 2
-		textblock tb 35 ty 15 2 "serif"
-		textblock tb 65 ty 10 2 "mono" "red"
+		textblock tb tx1 ty 20 2
+		textblock tb tx2 ty 15 2 "serif"
+		textblock tb tx3 ty 10 2 "mono" "red"
 		
-		textfile "AAPL.d" 5  50 2
-		textfile "AAPL.d" 35 50 2 "serif"
-		textfile "AAPL.d" 65 50 2 "mono" "red"
+		textfile "AAPL.d" tx1 50 2
+		textfile "AAPL.d" tx2 50 2 "serif"
+		textfile "AAPL.d" tx3 50 2 "mono" "red"
 	
-		textcode "code/hw.go" 5  75 20 1
-		textcode "code/hw.go" 35 75 20 1 "red"
+		textcode "code/hw.go" tx1 75 20 1
+		textcode "code/hw.go" tx2 75 20 1 "red"
 	slide end
 
+	l1=20
+	l2=50
+	l3=80
+	op1=70
+	op2=50
+	op3=30
+	ts1=4
 	slide begin "rgb(240,240,240)"
 		line l1 0 l1 100 0.1
 		line l2 0 l2 100 0.1
@@ -54,6 +57,7 @@ deck begin
 	s2=20
 	imfile="follow.jpg"
 	imlink="https://budnitzbicycles.com"
+	// Images
 	slide begin
 		image imfile midx midy iw ih
 		image imfile midx midy iw ih s1
@@ -70,74 +74,79 @@ deck begin
 	lx1=20
 	lx2=40
 	lx3=60
+	ly1=90
+	ly2=70
+	ly3=50
+	ly4=30
+	// Lists
 	slide begin
-		list lx1 90 lsize
+		list lx1 ly1 lsize
 			li "one"
 			li "two"
 			li "three"
 		elist
 		
-		blist lx2 90 lsize
+		blist lx2 ly1 lsize
 			li "one"
 			li "two"
 			li "three"
 		elist
 		
-		nlist lx3 90 lsize
+		nlist lx3 ly1 lsize
 			li "one"
 			li "two"
 			li "three"
 		elist
 		
-		list lx1 70 lsize "sans"
+		list lx1 ly2 lsize "sans"
 			li "one"
 			li "two"
 			li "three"
 		elist
 		
-		blist lx2 70 lsize "serif"
+		blist lx2 ly2 lsize "serif"
 			li "one"
 			li "two"
 			li "three"
 		elist
 		
-		nlist lx3 70 lsize "mono"
+		nlist lx3 ly2 lsize "mono"
 			li "one"
 			li "two"
 			li "three"
 		elist
 		
-		list lx1 50 lsize "sans" "red"
+		list lx1 ly3 lsize "sans" "red"
 			li "one"
 			li "two"
 			li "three"
 		elist
 		
-		blist lx2 50 lsize "serif" "green"
+		blist lx2 ly3 lsize "serif" "green"
 			li "one"
 			li "two"
 			li "three"
 		elist
 		
-		nlist lx3 50 lsize "mono" "blue"
+		nlist lx3 ly3 lsize "mono" "blue"
 			li "one"
 			li "two"
 			li "three"
 		elist
 		
-		list lx1 30 lsize "sans" "red" op1
+		list lx1 ly4 lsize "sans" "red" op1
 			li "one"
 			li "two"
 			li "three"
 		elist
 		
-		blist lx2 30 lsize "serif" "green" op2
+		blist lx2 ly4 lsize "serif" "green" op2
 			li "one"
 			li "two"
 			li "three"
 		elist
 		
-		nlist lx3 30 lsize "mono" "blue" op3
+		nlist lx3 ly4 lsize "mono" "blue" op3
 			li "one"
 			li "two"
 			li "three"
@@ -147,45 +156,46 @@ deck begin
 	c1="red"
 	c2="blue"
 	c3="green"
+	shapeop=30
+	// Shapes
 	slide begin
-		polygon	"15 20 25" "90 95 90"
-		polygon	"35 40 45" "90 95 90" c1
-		polygon	"55 60 65" "90 95 90" c2 30
+		polygon	   "15 20 25" "90 95 90"
+		polygon	   "35 40 45" "90 95 90" c1
+		polygon	   "55 60 65" "90 95 90" c2 shapeop 
 		
-		rect	l1 80 10 5
-		rect	40 80 10 5 c1
-		rect	60 80 10 5 c2 30
+		rect	   l1 80 10 5
+		rect	   40 80 10 5 c1
+		rect	   60 80 10 5 c2 shapeop 
 		
-		square	l1 70 5
-		square	40 70 5 c1
-		square	60 70 5 c2 30
+		square	   l1 70 5
+		square	   40 70 5 c1
+		square	   60 70 5 c2 shapeop 
 		
-		ellipse	l1 60 10 5
-		ellipse	40 60 10 5 c1
-		ellipse	60 60 10 5 c2 30
+		ellipse	   l1 60 10 5
+		ellipse	   40 60 10 5 c1
+		ellipse	   60 60 10 5 c2 shapeop 
 		
-		circle	l1 50 5
-		circle	40 50 5 c1
-		circle	60 50 5 c2 30
+		circle	   l1 50 5
+		circle	   40 50 5 c1
+		circle	   60 50 5 c2 shapeop 
 		
-		line	15 35 25 40
-		line	35 35 45 40 1 c1
-		line	55 35 65 40 1 c2
-		line	75 35 85 40 1 c3 30
+		line	   15 35 25 40
+		line	   35 35 45 40 1 c1
+		line	   55 35 65 40 1 c2
+		line	   75 35 85 40 1 c3 shapeop 
 		
-		arc	    20 25 10 5 0 180
-		arc	    40 25 10 5 0 180 1 c1
-		arc	    60 25 10 5 0 180 1 c2
-		arc	    80 25 10 5 0 180 1 c3 30
+		arc        20 25 10 5 0 180
+		arc        40 25 10 5 0 180 1 c1
+		arc        60 25 10 5 0 180 1 c2
+		arc        80 25 10 5 0 180 1 c3 shapeop 
 		
-		curve	15 15 10 25 25 15
-		curve	35 15 30 25 45 15 1
-		curve	55 15 45 25 65 15 1 c2
-		curve	75 15 65 25 85 15 1 c3 30
+		curve	   15 15 10 25 25 15
+		curve	   35 15 30 25 45 15 1
+		curve	   55 15 45 25 65 15 1 c2
+		curve	   75 15 65 25 85 15 1 c3 shapeop 
 	slide end
 	
 	// Example deck
-
 	imscale=58
 	dtop=87
 	chartleft=10
@@ -196,8 +206,8 @@ deck begin
 	opts="-fulldeck=f -textsize 1  -xlabel=2  -barwidth 1.5"
 
 	slide begin "white" "black"
-		ctext "Deck elements" 50 dtop 5
-		cimage "follow.jpg" "Dreams" 72 imy iw ih imscale imlink
+		ctext     "Deck elements" 50 dtop 5
+		cimage    "follow.jpg" "Dreams" 72 imy iw ih imscale imlink
 		textblock "Budnitz #1, Plainfield, NJ, May 10, 2015" 55 35 10 1 "serif" "white"
 
 		// List
@@ -223,8 +233,8 @@ deck begin
 	
 	
 	slide begin "white" "black"
-		ctext "Deck elements" 50 dtop 5
-		cimage "follow.jpg" "Dreams" 72 imy iw ih imscale imlink
+		ctext     "Deck elements" 50 dtop 5
+		cimage    "follow.jpg" "Dreams" 72 imy iw ih imscale imlink
 		textblock "Budnitz #1, Plainfield, NJ, May 10, 2015" 55 35 10 1 "serif" "white"
 
 		// List
