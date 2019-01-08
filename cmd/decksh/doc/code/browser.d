4c2c056d43eca974f452c479a4729725066a94f0
Anthony Starks <ajstarks@gmail.com>
2019-01-08 00:33:19 -0500

    decksh: doc: add more examples, include BBEdit code module

---
+++ cmd/decksh/doc/code/browser.d
@@ -0,0 +1,8 @@
+# Browser Market Share Dec 2016-Dec 2017
+Chrome	53.72	maroon
+Safari	14.47	blue
+Other	9.36	lightgray
+UC	8.28		purple
+Firefox	6.23	bluegray
+IE	3.99	green
+Opera	3.9	red
---
+++ cmd/decksh/doc/code/incar.d
@@ -0,0 +1,5 @@
+# US Incarceration Rate
+White	39	antiquewhite
+Hispanic	19	burlywood
+Black	40	sienna
+Other	2	gray
