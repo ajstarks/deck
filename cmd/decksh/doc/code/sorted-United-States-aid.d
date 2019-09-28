48686d729697b15f1a19d30def427dbb04eeda72
Anthony Starks <ajstarks@gmail.com>
2019-09-28 08:56:01 -0400

    decksh doc: add data files

---
+++ cmd/decksh/doc/code/sorted-United-States-aid.d
@@ -0,0 +1,10 @@
+Administrative costs 	1092
+Multisector aid 	727
+Std control including hiv/aids 	562
+Bio-diversity 	521
+Industrial development 	425
+STD control including HIV/AIDS 	372
+Strengthening civil society 	368
+Social/ welfare services 	351
+Material relief assistance and services 	332
+Air transport 	294
---
+++ cmd/decksh/doc/code/sorted-total-United-States-recip.d
@@ -0,0 +1,42 @@
+Colombia	17.1042
+India	16.5298
+Brazil	13.5483
+Korea	11.1544
+Kuwait	7.72742
+South Africa	6.67878
+Thailand	6.49564
+Saudi Arabia	4.44001
+Canada	4.0686
+Chile	3.69068
+Taiwan	2.83103
+Australia	2.57262
+Poland	2.50855
+Italy	2.40003
+Japan	2.08098
+Hungary	1.7653
+Romania	1.68628
+Czech Republic	1.25006
+United Kingdom	0.957344
+Ireland	0.934639
+Austria	0.769909
+Qatar	0.723167
+Spain	0.561712
+Luxembourg	0.55504
+Greece	0.519378
+Cyprus	0.49402
+Norway	0.368002
+France	0.237533
+New Zealand	0.225588
+Belgium	0.184346
+Lithuania	0.182454
+Finland	0.180622
+Portugal	0.17513
+Denmark	0.166311
+Latvia	0.156605
+Slovak Republic	0.0941139
+Sweden	0.0597572
+Estonia	0.0531269
+Liechtenstein	0.0182644
+Iceland	0.01806
+Slovenia	0.00691716
+United Arab Emirates	0.00154426
