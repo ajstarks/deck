4dca2a47ccee7ed9107644980f499a8ec88c2945
Anthony Starks <ajstarks@gmail.com>
2020-01-15 17:04:42 -0500

    decksh: clean up grid example in the test file

--- cmd/decksh/grid.d
+++ cmd/decksh/grid.d
@@ -4,12 +4,15 @@ circle x y 4
 circle x y 4
 circle x y 2
 circle x y 1
+
 arc x y 3 3 0 90
 arc x y 3 3 90 180
 arc x y 3 3 180 270
+
 square x y 4 "red"
 square x y 4 "green"
 square x y 4 "blue"
+
 image "follow.jpg" x y 640 480 10
 image "follow.jpg" x y 640 480 10
 image "follow.jpg" x y 640 480 10
