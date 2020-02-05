9cf3af6100e77504d4a0c1e495ef67a910503f63
Anthony Starks <ajstarks@gmail.com>
2020-02-04 20:57:17 -0500

    decksh doc: add gridex.d

---
+++ cmd/decksh/doc/code/gridex.d
@@ -0,0 +1,24 @@
+circle x y 1
+circle x y 2
+circle x y 4
+circle x y 8
+
+circle x y 8
+circle x y 4
+circle x y 2
+circle x y 1
+
+arc x y 3 3   0  90
+arc x y 3 3  90 180
+arc x y 3 3 180 270
+arc x y 3 3 270 360
+
+square x y 4 "red"
+square x y 4 "green"
+square x y 4 "blue"
+square x y 4 "orange"
+
+image "images/follow.jpg" x y 640 480 7
+image "images/cloudy.jpg" x y 640 480 7
+image "images/hearts.jpg" x y 640 480 7
+image "images/oldfields.jpg" x y 640 480 7
