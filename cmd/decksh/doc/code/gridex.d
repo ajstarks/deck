2cad449a9b9b8499cd430c09a0cafa1f361e8b01
Anthony Starks <ajstarks@gmail.com>
2020-02-16 13:04:39 -0500

    decksh doc: standardize on keyword placement

--- cmd/decksh/doc/code/gridex.d
+++ cmd/decksh/doc/code/gridex.d
@@ -13,11 +13,6 @@ arc x y 3 3  90 180
 arc x y 3 3 180 270
 arc x y 3 3 270 360
 
-square x y 4 "red"
-square x y 4 "green"
-square x y 4 "blue"
-square x y 4 "orange"
-
 image "images/follow.jpg" x y 640 480 7
 image "images/cloudy.jpg" x y 640 480 7
 image "images/hearts.jpg" x y 640 480 7
